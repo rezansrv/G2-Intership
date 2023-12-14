8993dab621273e745e67155157750c0e06c6f7d8
Reza Naserivand <rnaserivand@gmail.com>
2023-12-14 17:43:16 +0330

    course page is done

--- components/utils/navbar.vue
+++ components/utils/navbar.vue
@@ -2,14 +2,18 @@
   <header class="flex justify-center">
     <nav dir="rtl" class="flex justify-between flex-row items-center h-[100px] w-[1280px]">
       <div class="flex gap-10">
-        <img src="https://g2internship.com/_nuxt/img/Logo.2b62930.svg" alt="logo">
+        <nuxt-link to="/">
+          <img src="https://g2internship.com/_nuxt/img/Logo.2b62930.svg" alt="logo">
+        </nuxt-link>
         <ul class="flex items-center gap-7 text-[17px] font-medium">
-          <li><a href="#">خانه</a></li>
+          <nuxt-link to="/">خانه</nuxt-link>
           <li><a href="#">درباره ما</a></li>
-          <li><a href="#">دوره ها</a></li>
+          <nuxt-link to="courses">دوره ها</nuxt-link>
         </ul>
       </div>
-      <button class="bg-primary font-medium text-white rounded-full items-center h-[40px] w-[110px]">ورود | ثبت نام</button>
+      <nuxt-link to="account/login">
+        <button class="bg-primary font-medium text-white rounded-full items-center h-[40px] w-[110px]">ورود | ثبت نام</button>
+      </nuxt-link>
     </nav>
   </header>
 </template>
---
+++ pages/courses.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <courses/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import courses from '~/components/courses/courses.vue'
+
+@Component({
+  name: "signUp",
+  components:
+    {
+      courses
+    },
+  layout:"defaultLay"
+
+
+
+})
+export default class Account extends Vue {
+  mounted() {
+    console.log('hi')
+  }
+}
+</script>
